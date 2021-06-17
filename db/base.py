7589fd29cc6c2affe8904b184bb9b990582cddc6
Ion <mindruyon@mail.ru>
2021-06-17 12:55:29 +0300

    first commit

---
+++ db/base.py
@@ -0,0 +1,2 @@
+from db.base_class import Base  # noqa
+from models.user import User  # noqa
---
+++ db/init_db.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import crud
+from starlette import schemas
+
+from core.config import settings
+from app.db import base  # noqa: F401
+
+
+# make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
+# otherwise, SQL Alchemy might fail to initialize relationships properly
+# for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
+
+
+def init_db(db: Session) -> None:
+    ...
